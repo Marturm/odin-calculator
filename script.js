5c3dd08f08892426b5ee3175588305338b2cd94e
Marturm <86203188+Marturm@users.noreply.github.com>
2024-04-11 20:03:20 +0200

    create variables for each button

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let a, b, operator;
+let a, b, operator, displayValue;
+
+const display = document.querySelector(".display");
+
+function populateDisplay(value) {
+    display.innerText = value;
+}
 
 function add(a, b) {
     return a + b;
@@ -28,3 +34,28 @@ function operate(a, b, operator) {
             return divide(a, b);
     }
 }
+
+
+const btnNine = document.querySelector(".button-nine");
+const btnEight = document.querySelector(".button-eight");
+const btnSeven = document.querySelector(".button-seven");
+const btnSix = document.querySelector(".button-six");
+const btnFive = document.querySelector(".button-five");
+const btnFour = document.querySelector(".button-four");
+const btnThree = document.querySelector(".button-three");
+const btnTwo = document.querySelector(".button-two");
+const btnOne = document.querySelector(".button-one");
+const btnZero = document.querySelector(".button-zero");
+
+const btnAC = document.querySelector(".button-ac");
+
+const btnEqual = document.querySelector(".button-equal");
+
+const btnAdd = document.querySelector(".button-add");
+const btnMultiply = document.querySelector(".button-multiply");
+const btnSubtract = document.querySelector(".button-subtract");
+const btnDivide = document.querySelector(".button-divide");
+
+const btnPoint = document.querySelector(".button-point");
+
+
